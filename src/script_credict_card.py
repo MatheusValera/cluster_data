a668d3b02877cecc9a00099417aa59e4570a8053
MatheusValera <lrfp1000@.gmail.com>
2022-11-04 16:27:33 -0300

    feat: add graphs visualization with best clusters

--- src/script_credict_card.py
+++ src/script_credict_card.py
@@ -27,7 +27,7 @@ def clustering_algorithm(n_clusters, df):
   
   kmeans = KMeans(n_clusters, n_init = 10, max_iter = 300)
   labels = kmeans.fit_predict(df)
-  labels_final = kmeans.labels_
+
   # calculate silhouette with data frame and labels, best values > 0 (positive)
   silhouette = metrics.silhouette_score(df, labels, metric = 'euclidean')
   
@@ -39,16 +39,16 @@ def clustering_algorithm(n_clusters, df):
   
   print('Numero de clusters: ',n_clusters,silhouette, dbs, calinski)
   
-  return silhouette, dbs, calinski, labels_final
+  return silhouette, dbs, calinski, kmeans
 # %%
 # Make attempts with several cluster numbers, 
 # considering the value of Silhouette as the main one, 
 # the number that was better was with 5 cluster (Attempts = 3,5,10,20,50)
-silhouette_final, dbs_final, calinski_final, labels_final = clustering_algorithm(5, df_normalize)
+silhouette_final, dbs_final, calinski_final, kmeans_final = clustering_algorithm(5, df_normalize)
 # %%
 # create random data frame 
 random_data = np.random.rand(8950, 16)
-silhouette_random, dbs_random, calinski_random,labels_random = clustering_algorithm(5, random_data)
+clustering_algorithm(5, random_data)
 # %%
 # Validating the stability of our dataset checking if
 # splitting into 3 parts the results will be stable for each other
@@ -58,12 +58,29 @@ clustering_algorithm(5, set2)
 clustering_algorithm(5, set3)
 # %%
 # graphics
-plt.scatter(df['PURCHASES'], df['PAYMENTS'], c = labels_final, s = 5, cmap = 'rainbow')
+plt.scatter(df['PURCHASES'], df['PAYMENTS'], c = kmeans_final.labels_, s = 5, cmap = 'rainbow')
 plt.xlabel('Valor total pago')
 plt.ylabel('Valor total gasto')
 plt.show
 # %%
 # show all possibility graphs about all columns
-df['cluster'] = labels_final
+df['cluster'] = kmeans.labels_
 sns.pairplot(df[0:], hue = 'cluster')
+# %%
+# show variances by centroids
+centroids = kmeans_final.cluster_centers_
+print(centroids)
+# %%
+# get best variances
+max = len(centroids[0])
+for i in range(max):
+  if centroids[:, i].var() > 0.009:
+    print(df.columns.values[i], '{:.4f}'.format(centroids[:, i].var()))
+# %%
+# group by cluster
+description = df.groupby('cluster')['BALANCE', 'PURCHASES', 'CASH_ADVANCE', 'CREDIT_LIMIT', 'PAYMENTS']
+n_clients_cluster = description.size()
+description = description.mean()
+description['n_clients'] = n_clients_cluster
+print(description)
 # %%
