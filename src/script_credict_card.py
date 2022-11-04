61ab82dfa41c20d2ec26c124d957fe2ff0162672
MatheusValera <lrfp1000@.gmail.com>
2022-11-04 11:53:15 -0300

    refactor: add function to clustering by kmeans and attemps with best n clusters

--- src/script_credict_card.py
+++ src/script_credict_card.py
@@ -18,16 +18,26 @@ df.fillna(df.median(), inplace = True)
 #normalize data frame
 df_normalize = Normalizer().fit_transform(df.values)
 # %%
-# generate cluster
-kmeans = KMeans(n_clusters = 5, n_init = 10, max_iter = 300)
-y_pred = kmeans.fit_predict(df_normalize)
+def clustering_algorithm(n_clusters):
+  
+  kmeans = KMeans(n_clusters, n_init = 10, max_iter = 300)
+  labels = kmeans.fit_predict(df_normalize)
+  
+  # calculate silhouette with data frame and labels, best values > 0 (positive)
+  silhouette = metrics.silhouette_score(df_normalize, labels, metric = 'euclidean')
+  
+  # calculate index Davies Bouldin, best values next 0
+  dbs = metrics.davies_bouldin_score(df_normalize, labels)
+  
+  # calculate Calinski
+  calinski = metrics.calinski_harabasz_score(df_normalize, labels)
+  
+  print('Numero de clusters: ',n_clusters,silhouette, dbs, calinski)
+  
+  return silhouette, dbs, calinski
 # %%
-labels = kmeans.labels_
-# calculate silhouette with data frame and labels, best values > 0 (positive)
-result_silhouette = metrics.silhouette_samples(df_normalize, labels, metric = 'euc')
+#Make attempts with several cluster numbers, 
+# considering the value of Silhouette as the main one, 
+# the number that was better was with 5 cluster (Attempts = 3,5,10,20,50)
+clustering_algorithm(5)
 # %%
-# calculate index Davies Bouldin, best values next 0
-dbs = metrics.davies_bouldin_score(df_normalize, labels)
-# %%
-# calculate Calinski
-calinski = metrics.calinski_harabasz_score(df_normalize, labels)
